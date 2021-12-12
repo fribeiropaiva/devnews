bd6f1c55d8d342b514d6013f4f840041fdc8c8b3
Filipe Ribeiro <filipepaiva240@gmail.com>
2021-12-12 12:48:20 -0300

    Fix eslint

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,19 +1,17 @@
-import Document from 'next/document'
+import Document from 'next/document';
 
-export default class MyDocument extends Document{
+export default class MyDocument extends Document {
   render() {
     return (
       <html lang="en">
-      <head>
-        <meta charset="UTF-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Document</title>
-      </head>
-      <body>
-
-      </body>
+        <head>
+          <meta charset="UTF-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <title>Document</title>
+        </head>
+        <body />
       </html>
-    )
+    );
   }
 }
