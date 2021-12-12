b45f234f3a2cb390b0a1bf6747f92bc7f76f0aa7
Filipe Ribeiro <filipepaiva240@gmail.com>
2021-12-12 18:14:12 -0300

    Add styles to home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,31 @@
-import Head  from 'next/head';
+import Head from 'next/head';
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | Dev News</title>
+        <title>Home | Dev News</title>
       </Head>
-      <h1>Hello World</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.textContent}>
+          <span>👏 Hey, welcome!</span>
+          <h1>
+            News about the
+            {' '}
+            <span>coding</span>
+            {' '}
+            world!
+          </h1>
+          <p>
+            Get access to all publications
+            <br />
+            for
+            <span className={styles.subscriptionPrice}> $9.99 per month</span>
+          </p>
+        </section>
+        <img className={styles.banner} src="/images/girl-coding.svg" alt="girl coding" />
+      </main>
     </>
   );
 }
