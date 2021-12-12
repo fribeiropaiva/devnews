e9b24b6c6309606aa426a3287191c8318598dca4
Filipe Ribeiro <filipepaiva240@gmail.com>
2021-12-12 16:51:38 -0300

    Make header global

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
 import { AppProps } from 'next/app';
+import { Header } from '../components/Header';
+import '../styles/global.scss';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp = function ({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
+};
 
-export default MyApp
+export default MyApp;
