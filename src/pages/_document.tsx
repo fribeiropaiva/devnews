a5e31048ab1da614a42d5d61b90485fa5f91ac20
Filipe Ribeiro <filipepaiva240@gmail.com>
2021-12-12 13:13:57 -0300

    Fix _document structure and import fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,17 +1,21 @@
-import Document from 'next/document';
+import Document, {
+  Html, Head, Main, NextScript,
+} from 'next/document';
 
 export default class MyDocument extends Document {
   render() {
     return (
-      <html lang="en">
-        <head>
-          <meta charset="UTF-8" />
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <title>Document</title>
-        </head>
-        <body />
-      </html>
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
     );
   }
 }
